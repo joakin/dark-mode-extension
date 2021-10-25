7627c07a85ef2c4fc8979b0c33c842b4c51fa76a
joakin <joaquin@chimeces.com>
2021-10-25 19:31:26 +0200

    Invert images back to normal when dark mode is enabled

--- src/background.js
+++ src/background.js
@@ -34,14 +34,30 @@ chrome.action.onClicked.addListener(function (tab) {
 function enableDarkMode(tabId) {
   let html = document.documentElement;
   let bg = window.__extension__darkmode_html_bg || html.style.backgroundColor;
+  let style = document.querySelector("#__darkmode_styles");
+
+  if (!style) {
+    style = document.createElement("style");
+    style.id = "__darkmode_styles";
+    style.innerHTML = `
+html.dark-mode-enabled {
+  filter: hue-rotate(180deg) contrast(0.95) saturate(0.95) invert(0.90);
+}
+
+html.dark-mode-enabled img,
+html.dark-mode-enabled [style*="background-image:"] {
+  filter: invert(1) hue-rotate(180deg);
+}
+    `;
+    document.body.appendChild(style);
+  }
 
   if (!!window.__extension__darkmode) {
-    html.style.filter = "";
+    html.classList.remove("dark-mode-enabled");
     html.style.backgroundColor = bg;
     window.__extension__darkmode = false;
   } else {
-    html.style.filter =
-      "hue-rotate(180deg) contrast(0.95) saturate(0.95) invert(0.90)";
+    html.classList.add("dark-mode-enabled");
     html.style.backgroundColor = "#fff";
     window.__extension__darkmode = true;
   }
